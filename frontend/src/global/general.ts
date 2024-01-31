98a83b14e070f8bfde0f599da01b9316291b06cc
KlintLee115 <54990359+KlintLee115@users.noreply.github.com>
2024-01-31 08:06:05 -0700

    changed backend link

--- frontend/src/global/general.ts
+++ frontend/src/global/general.ts
@@ -1,7 +1,7 @@
 import { create } from 'zustand'
 
-const backendHost = "http://localhost:3000/"
-// const backendHost = "https://ksportsbackend.onrender.com/ksports/"
+// const backendHost = "http://localhost:3000/"
+const backendHost = "https://ksportsbackend.onrender.com/"
 
 export const PRICE_SORT = {
     LOW_TO_HIGH: "LOW_TO_HIGH",
