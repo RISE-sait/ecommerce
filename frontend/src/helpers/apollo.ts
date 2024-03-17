537de7408182c11d623b60910c7496f6e06653bc
KlintLee115 <54990359+KlintLee115@users.noreply.github.com>
2024-03-16 21:38:14 -0600

    fixed environment issue

--- frontend/src/helpers/apollo.ts
+++ frontend/src/helpers/apollo.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: process.env.VERCEL_ENV === "production" ? "https://ksportsbackend.azurewebsites.net/graphql" : "http://localhost:3000/graphql",
+  uri: process.env.NODE_ENV === "production" ? "https://ksportsbackend.azurewebsites.net/graphql" : "http://localhost:3000/graphql",
   cache: new InMemoryCache(),
 });
 
