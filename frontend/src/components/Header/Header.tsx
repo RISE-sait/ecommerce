a78e02f6885aa10b2ecf08a0646667f24c0b36b1
KlintLee115 <klintlee1@gmail.com>
2024-09-23 10:36:52 -0600

    fixed header issues

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -19,13 +19,13 @@ const NavLinks: NavLinkProp[] = [
 export default function Header() {
 
   return (
-    <header className="w-full bg-[#F5F5F3] sticky top-0 z-10">
-      <div className="max-w-container mx-auto flex items-center justify-between px-4 pb-4 lg:pb-0 h-24">
+    <header className="bg-[#F5F5F3] sticky top-0 z-10">
+      <div className="flex items-center justify-between px-10 py-5">
         <Link href="/">
           <h3 className="bg-blue-500 text-white font-bold text-md md:text-xl px-3 py-2">KSPORTS</h3>
         </Link>
-        <SearchBar/>
-        <div className="w-1/3 flex items-center">
+        <SearchBar />
+        <div className="hidden md:flex items-center">
           {NavLinks.map(({ title, link }) => (
             <Link
               key={title}
@@ -39,9 +39,10 @@ export default function Header() {
 
           {/* Larger devices display */}
           <UserOptions />
-          {/* Phones display */}
-          <HamburgerMenuIcon />
         </div>
+
+        {/* Phones display */}
+        <HamburgerMenuIcon />
       </div>
     </header>
   );
